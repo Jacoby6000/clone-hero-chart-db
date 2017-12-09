b95776b77a19754e514d5754d38d5cd0c5e5a1f7
Jacob Barber <jacob.barber@stackpath.com>
2017-12-08 22:17:47 -0600

    Get configuration loading in the build sbt, and flyway leveraging database config.

--- build.sbt
+++ build.sbt
@@ -1,4 +1,20 @@
+import java.nio.file.Paths
+
 import com.jacoby6000.cloneherodb.build.settings._
+import com.jacoby6000.cloneherodb.config._
+
+
+lazy val dbConfig =
+  loadCloneHeroDbConfig(Paths.get("src/main/resources/reference.conf"))
+    .leftMap(failuresToErrorMessage)
+    .leftMap("\n\n" + _)
+    .map(_.database)
+    .fold(sys.error(_), identity)
+
+lazy val dbHost = dbConfig.host
+lazy val dbPort = dbConfig.port
+lazy val db = dbConfig.databaseName
+lazy val user = dbConfig.username
 
 lazy val server = (project in file("."))
   .dependsOn(config)
@@ -8,17 +24,14 @@ lazy val server = (project in file("."))
   .settings(
     resolvers += "jmcardon at bintray" at "https://dl.bintray.com/jmcardon/tsec",
 
-    libraryDependencies ++= rootProjectDependencies
+    libraryDependencies ++= rootProjectDependencies,
+
+    flywayUrl := s"jdbc:postgresql://$dbHost:$dbPort/$db",
+    flywayUser := user
   )
 
 // configuration sub-project build located in <clone-hero-chart-db>/project/metabuild.sbt
 lazy val config = ProjectRef(file("project"), "config")
 
-val dbHost = "localhost"
-val dbPort = 5432
-val db = "clone_hero_db"
-val user = "clone_hero_db"
 
-flywayUrl := s"jdbc:postgresql://$dbHost:$dbPort/$db"
-flywayUser := user
 
---
+++ config/src/main/scala/com/jacoby6000/cloneherodb/config.scala
@@ -0,0 +1,54 @@
+package com.jacoby6000.cloneherodb
+
+import java.nio.file.Path
+
+import pureconfig._
+import pureconfig.error.ConfigReaderFailures
+
+import scala.concurrent.duration.FiniteDuration
+import scalaz._
+import Scalaz._
+
+object config {
+
+  def loadCloneHeroDbConfig(configPath: Path): ConfigReaderFailures \/ CloneHeroDbConfiguration =
+    loadConfig[CloneHeroDbConfiguration](
+      path = configPath,
+      namespace = "clone-hero-db"
+    ).disjunction
+
+  def failuresToErrorMessage(configReaderFailures: ConfigReaderFailures): String =
+    configReaderFailures.toList.foldLeft("Failed to read configuration:\n") { (acc, err) =>
+      acc + "\t" + err.location.cata(
+        v => v.description,
+        "Config path unknown"
+      ) + ": " + err.description + "\n"
+    }
+
+  case class CloneHeroDbConfiguration(
+    database: DatabaseConfiguration,
+    google: GoogleApiConfiguration
+  )
+
+  case class GoogleApiConfiguration(
+    oauth: GoogleOAuthApiConfiguration,
+    drive: GoogleDriveApiConfiguration
+  )
+
+  case class GoogleOAuthApiConfiguration(
+    newTokenAfter: FiniteDuration,
+    credentialsJsonLocation: String
+  )
+
+  case class GoogleDriveApiConfiguration(
+    requestThrottleDelay: FiniteDuration
+  )
+
+  case class DatabaseConfiguration(
+    host: String,
+    port: Int,
+    databaseName: String,
+    username: String,
+    password: Option[String]
+  )
+}
