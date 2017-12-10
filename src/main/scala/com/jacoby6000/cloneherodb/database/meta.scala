094bd2de6fb7330179cee2b83afbf10990f4a191
Jacob Barber <jacob.barber@stackpath.com>
2017-12-09 18:25:40 -0600

    Did bad things for doobie

--- src/main/scala/com/jacoby6000/cloneherodb/database/meta.scala
+++ src/main/scala/com/jacoby6000/cloneherodb/database/meta.scala
@@ -13,17 +13,26 @@ object meta {
     def maybe: ConnectionIO[Maybe[A]] = query.option.map(Maybe.fromOption)
   }
 
-  implicit def maybeComposite[A](implicit composite: Composite[Option[A]]): Composite[Maybe[A]] =
-    composite.imap(Maybe.fromOption)(_.toOption)
+  implicit def maybeComposite[A](implicit ev: Meta[A]): Composite[Maybe[A]] =
+    Composite.fromMetaOption(ev).imap(Maybe.fromOption)(_.toOption)
+
+  /** This violates the design philosophy. Assuming this doesn't break anything, I'm going to file
+    * an issue to doobie so that we can make Param instances without cheating.
+    */
+  implicit def maybeParam[A](implicit ev: Meta[A]): Param[Maybe[A]] = {
+    val constructor = classOf[Param[Maybe[A]]].getConstructors.head
+    constructor.setAccessible(true)
+    constructor.newInstance(maybeComposite(ev)).asInstanceOf[Param[Maybe[A]]]
+  }
 
   implicit val songNameMeta: Meta[SongName] =
-    Meta[String].xmap[SongName](SongName(_), _.value)
+    Meta[String].xmap[SongName](SongName, _.value)
 
   implicit def entityIdMeta[A, B: Meta](implicit tt: TypeTag[EntityId[A, B]]): Meta[EntityId[A, B]] =
     Meta[B].xmap[EntityId[A, B]](EntityId(_), _.value)
 
   implicit val fileTypeMeta: Meta[FileType] =
-    Meta[String].xmap[FileType](FileType.withName(_), _.entryName)
+    Meta[String].xmap[FileType](FileType.withName, _.entryName)
 
   implicit val apiKeyMeta: Meta[ApiKey] = {
     sealed trait ApiKeyType extends EnumEntry
