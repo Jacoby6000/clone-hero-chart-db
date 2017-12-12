dac9c46d88baf8bcf76575460dcbaa3f5555b2d7
Jacoby6000 <Jacoby6000@gmail.com>
2017-12-11 20:34:16 -0600

    Codecov.io support

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sbt ++$TRAVIS_SCALA_VERSION flywayMigrate compile test it:test
+sbt ++$TRAVIS_SCALA_VERSION flywayMigrate compile coverage test it:test coverageReport
+bash <(curl -s https://codecov.io/bash)
