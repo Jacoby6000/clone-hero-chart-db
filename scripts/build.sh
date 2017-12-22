1e837a8a07060b0010e81012a4477f2b2a387043
Jacob Barber <Jacoby6000@gmail.com>
2017-12-22 03:14:44 -0600

    Fix always passing builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sbt ++$TRAVIS_SCALA_VERSION flywayMigrate compile coverage test it:test coverageReport
-bash <(curl -s https://codecov.io/bash)
+sbt ++$TRAVIS_SCALA_VERSION flywayMigrate compile coverage test it:test coverageReport && bash <(curl -s https://codecov.io/bash)
